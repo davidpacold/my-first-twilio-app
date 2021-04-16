95ff87521357817c5e11cb062fa9b3a0a7a0432c
David Pacold <dpacold@gmail.com>
2021-04-15 21:06:39 -0400

    updated to a working app file
    had to rewrite due to changes made as i was writing up the readme

--- Files/numberguess.js
+++ Files/numberguess.js
@@ -1,57 +1,57 @@
 const express = require('express');
-const urlencoded = require('body-parser').urlencoded;
+const bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//define a variable for the start game incoming message
-StartGame = 'Start'
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
 
-// defines  a function to generate a random number
-function getRandomInt(max){
-  return Math.floor(Math.random() * max);
-
-// Parse incoming POST params with Express middleware
-app.use(urlencoded({ extended: false }));
-const app = express();
-
-// Create an HTTP server and listen for requests on port 3000
-app.listen(3000, () => {
-    console.log(
-      'Now listening on port 3000. ' +
-      'Be sure to restart when you make code changes!'
-    );
-  });
-
-// Create a route that will handle Twilio webhook requests, sent as an
-// HTTP POST to /inbound in our application
-app.post('/numberguess', (request, response) => {})
-  // Get information about the incoming call
-  // Collecting the body of the message to determine the guess
-  const incomingBody = request.body.Body;
-  if(incomingBody == StartGame)
-  {
-  console.log('Game Starting');
+app.post('/numberguess', (request, response) => {
+  //Send a response to the user welcomeing them with instructions
+  //res.send("<Response><Message>The game is starting, a number from 0 - 3 has been selected, try to guess it</Message></Response>")
+  //res.send('Hello numberguess!');
+  //Define Start Game verb to begin generate a new number
+StartGame='Start';
+const incomingBody = request.body.Body;
+console.log('User has submitted '+incomingBody);
+if(incomingBody == StartGame)
+{
   gamenumber = getRandomInt(9);
-  console.log(gamenumber)
-  response.send( "<Response><Message>The game is starting, a number from 0 - 9 has been selected, try to guess it</Message></Response>");
-  } 
-  else if(incomingBody < gamenumber)
-  {
-  console.log('Too low, aim higher')
-  response.send( "<Response><Message>Good guess, but that was too low, aim higher</Message></Response>");
-  }
-  else if(incomingBody > gamenumber)
-  {
-  console.log('Too high, aim lower')
-  response.send( "<Response><Message>Good guess, but that was too high, aim lower</Message></Response>");
-  }
-  else if(incomingBody == gamenumber)
-  {
-  console.log('Nailed it!')
-  response.send( "<Response><Message>Good guess, you nailed it! Start another game by saying 'Start'</Message></Response>");
-  }
-  else
-  {
-  console.log('unknown entry')
-  response.send( "<Response><Message>im not sure what you mean, say 'Start' to begin a game</Message></Response>");
+  //Define a function to generate a random number
+  function getRandomInt(max){
+    return Math.floor(Math.random() * max);
   }
 
+console.log('Game Starting');
+console.log('the chosen number is '+gamenumber);
+response.send( "<Response><Message>The game is starting, a number from 0 - 9 has been selected, try to guess it</Message></Response>");
+} 
+else if(incomingBody < gamenumber)
+{
+console.log('Too low, aim higher')
+response.send( "<Response><Message>Good guess, but that was too low, aim higher</Message></Response>");
 }
+else if(incomingBody > gamenumber)
+{
+console.log('Too high, aim lower')
+response.send( "<Response><Message>Good guess, but that was too high, aim lower</Message></Response>");
+}
+else if(incomingBody == gamenumber)
+{
+console.log('Nailed it!')
+response.send( "<Response><Message>Good guess, you nailed it!</Message></Response>");
+}
+else
+{
+console.log('unknown entry')
+response.send( "<Response><Message>im not sure what you mean, say 'Start' to begin a game</Message></Response>");
+}
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
